39a22014260fefaddf06fafe97bfc0dd80624178
Hugo Montan <83455566+hugofmontan@users.noreply.github.com>
2023-09-03 02:33:31 -0300

    Update barchart.py

--- Data_Extraction/barchart.py
+++ Data_Extraction/barchart.py
@@ -2,34 +2,37 @@ import pandas as pd
 import plotly.graph_objects as go
 
 def create_barchart(df):
-    df = pd.read_csv('prices.csv')
-
-    df.rename(columns={'ARB11841-USD': 'ARB'}, inplace=True)
-    df.rename(columns={'BTC-USD': 'BTC'}, inplace=True)
-    df.rename(columns={'DYDX-USD': 'DYDX'}, inplace=True)
-    df.rename(columns={'ETH-USD': 'ETH'}, inplace=True)
-    df.rename(columns={'LDO-USD': 'LDO'}, inplace=True)
-    df.rename(columns={'LINK-USD': 'LINK'}, inplace=True)
-    df.rename(columns={'MATIC-USD': 'MATIC'}, inplace=True)
-    df.rename(columns={'OP-USD': 'OP'}, inplace=True)
-    df.rename(columns={'PRIME23711-USD': 'PRIME'}, inplace=True)
-    df.rename(columns={'SOL-USD': 'SOL'}, inplace=True)
-    df.rename(columns={'STX4847-USD': 'STX'}, inplace=True)
-
+    # Assuming df is passed as a parameter
+    # No need to read the CSV again here
+    
+    df_renamed = df.rename(columns={
+        'ARB11841-USD': 'ARB',
+        'BTC-USD': 'BTC',
+        'DYDX-USD': 'DYDX',
+        'ETH-USD': 'ETH',
+        'LDO-USD': 'LDO',
+        'LINK-USD': 'LINK',
+        'MATIC-USD': 'MATIC',
+        'OP-USD': 'OP',
+        'PRIME23711-USD': 'PRIME',
+        'SOL-USD': 'SOL',
+        'STX4847-USD': 'STX'
+    })
+    
     tickers = ['BTC', 'ETH', 'LDO', 'MATIC', 'ARB', 'LINK', 'SOL', 'OP', 'STX', 'DYDX', 'PRIME']
-
-    df = df.drop('Datetime', axis=1)
-
-    for i in tickers: 
-        df[i] = df[i].pct_change().add(1).cumprod().sub(1) * 100
-
-    df = df.iloc[-1]
-    text_values = [f'{value:.2f}%' for value in df.round(2)]
+    
+    df_filtered = df_renamed[tickers]
+    
+    for ticker in tickers:
+        df_filtered[ticker] = (df_filtered[ticker].pct_change() + 1).cumprod().sub(1) * 100
+    
+    last_row = df_filtered.iloc[-1]
+    text_values = [f'{value:.2f}%' for value in last_row]
 
     # Create a Plotly bar chart for the last performance
     fig = go.Figure(go.Bar(
-        x=df.index,
-        y=df,
+        x=last_row.index,
+        y=last_row,
         marker_color='#623AD7',
         text=text_values,
         textposition='outside'
